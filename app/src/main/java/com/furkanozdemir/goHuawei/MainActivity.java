5aa8674acd64bf1ed495de08b1717e823388e216
ozdemirfurkan <furkan.ozdemir1@boun.edu.tr>
2021-01-06 21:28:13 +0300

    Initial commit

---
+++ app/src/main/java/com/furkanozdemir/goHuawei/MainActivity.java
@@ -0,0 +1,448 @@
+package com.furkanozdemir.goHuawei;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.common.ResolvableApiException;
+import com.huawei.hms.location.FusedLocationProviderClient;
+import com.huawei.hms.location.LocationAvailability;
+import com.huawei.hms.location.LocationCallback;
+import com.huawei.hms.location.LocationRequest;
+import com.huawei.hms.location.LocationResult;
+import com.huawei.hms.location.LocationServices;
+import com.huawei.hms.location.LocationSettingsRequest;
+import com.huawei.hms.location.LocationSettingsResponse;
+import com.huawei.hms.location.LocationSettingsStatusCodes;
+import com.huawei.hms.location.SettingsClient;
+import com.huawei.hms.maps.CameraUpdate;
+import com.huawei.hms.maps.CameraUpdateFactory;
+import com.huawei.hms.maps.HuaweiMap;
+import com.huawei.hms.maps.OnMapReadyCallback;
+import com.huawei.hms.maps.SupportMapFragment;
+import com.huawei.hms.maps.model.LatLng;
+import com.huawei.hms.maps.model.LatLngBounds;
+import com.huawei.hms.maps.model.Marker;
+import com.huawei.hms.maps.model.MarkerOptions;
+import com.huawei.hms.maps.model.Polyline;
+import com.huawei.hms.maps.model.PolylineOptions;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final String TAG = "MapDemo";
+    private SupportMapFragment mSupportMapFragment;
+    private HuaweiMap hMap;
+    private Marker mMarkerOrigin;
+    private Marker mMarkerDestination;
+
+    private static Double lat = 41.0;
+    private static Double lon = 29.2;
+    private final LatLng huawei = new LatLng(41.028880,29.117224);
+
+    private List<Polyline> mPolylines = new ArrayList<>();
+    private List<List<LatLng>> mPaths = new ArrayList<>();
+    private LatLngBounds mLatLngBounds;
+
+    FusedLocationProviderClient mFusedLocationProviderClient;
+    SettingsClient mSettingsClient;
+    LocationCallback mLocationCallback;
+    LocationRequest mLocationRequest;
+
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    renderRoute(mPaths, mLatLngBounds);
+                    break;
+                case 1:
+                    Bundle bundle = msg.getData();
+                    String errorMsg = bundle.getString("errorMsg");
+                    Toast.makeText(MainActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        if (fragment instanceof SupportMapFragment) {
+            mSupportMapFragment = (SupportMapFragment) fragment;
+            mSupportMapFragment.getMapAsync(this);
+        }
+        CheckPermissions();
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(20000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (null == mLocationCallback) {
+            mLocationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult != null) {
+                        List<Location> locations = locationResult.getLocations();
+                        if (!locations.isEmpty()) {
+                            for (Location location : locations) {
+                                lat = location.getLatitude();
+                                lon = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+
+                @Override
+                public void onLocationAvailability(LocationAvailability locationAvailability) {
+                    if (locationAvailability != null) {
+                        boolean flag = locationAvailability.isLocationAvailable();
+                        Log.i(TAG, "onLocationAvailability isLocationAvailable:" + flag);
+                    }
+                }
+            };
+        }
+        requestLocationUpdatesWithCallback();
+    }
+
+
+    @Override
+    public void onMapReady(HuaweiMap huaweiMap) {
+        hMap = huaweiMap;
+        hMap.setMyLocationEnabled(true);
+        hMap.getUiSettings().setCompassEnabled(true);
+        hMap.getUiSettings().setMyLocationButtonEnabled(true);
+        hMap.moveCamera(CameraUpdateFactory.newLatLngZoom(huawei, 13));
+        addDestinationMarker(huawei);
+    }
+
+    private void requestLocationUpdatesWithCallback() {
+        Log.i(TAG, "requestLocationUpdatesWithCallback");
+        try {
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+            builder.addLocationRequest(mLocationRequest);
+            LocationSettingsRequest locationSettingsRequest = builder.build();
+            Task<LocationSettingsResponse> locationSettingsResponseTask = mSettingsClient.checkLocationSettings(locationSettingsRequest);
+            locationSettingsResponseTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+                @Override
+                public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                    Log.i(TAG, "check location settings success");
+                    mFusedLocationProviderClient
+                            .requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper())
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.i(TAG, "requestLocationUpdatesWithCallback onSuccess");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(Exception e) {
+                                    Log.e(TAG,
+                                            "requestLocationUpdatesWithCallback onFailure:" + e.getMessage());
+                                }
+                            });
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            Log.e(TAG, "checkLocationSetting onFailure:" + e.getMessage());
+                            int statusCode = ((ApiException) e).getStatusCode();
+                            switch (statusCode) {
+                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                    try {
+                                        // When the startResolutionForResult is invoked, a dialog box is displayed, asking you to open the corresponding permission.
+                                        ResolvableApiException rae = (ResolvableApiException) e;
+                                        rae.startResolutionForResult(MainActivity.this, 0);
+                                    } catch (IntentSender.SendIntentException sie) {
+                                        Log.e(TAG, "PendingIntent unable to execute request.");
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e(TAG, "requestLocationUpdatesWithCallback exception:" + e.getMessage());
+        }
+    }
+
+
+
+    public void getWalkingRouteResult(View view) {
+        Toast.makeText(MainActivity.this, "Loading...", Toast.LENGTH_SHORT).show();
+        removePolylines();
+        removeLocationUpdatesWithCallback();
+        NetworkRequestManager.getWalkingRoutePlanningResult(new LatLng(lat,lon), huawei,
+                new NetworkRequestManager.OnNetworkListener() {
+                    @Override
+                    public void requestSuccess(String result) {
+                        generateRoute(result);
+                    }
+
+                    @Override
+                    public void requestFail(String errorMsg) {
+                        Message msg = Message.obtain();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("errorMsg", errorMsg);
+                        msg.what = 1;
+                        msg.setData(bundle);
+                        mHandler.sendMessage(msg);
+                    }
+                });
+        requestLocationUpdatesWithCallback();
+    }
+
+    public void getBicyclingRouteResult(View view) {
+        Toast.makeText(MainActivity.this, "Loading...", Toast.LENGTH_SHORT).show();
+        removePolylines();
+        removeLocationUpdatesWithCallback();
+        NetworkRequestManager.getBicyclingRoutePlanningResult(new LatLng(lat,lon), huawei,
+                new NetworkRequestManager.OnNetworkListener() {
+                    @Override
+                    public void requestSuccess(String result) {
+                        generateRoute(result);
+                    }
+
+                    @Override
+                    public void requestFail(String errorMsg) {
+                        Message msg = Message.obtain();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("errorMsg", errorMsg);
+                        Log.d("sfj", errorMsg);
+                        msg.what = 1;
+                        msg.setData(bundle);
+                        mHandler.sendMessage(msg);
+                    }
+                });
+        requestLocationUpdatesWithCallback();
+    }
+
+    public void getDrivingRouteResult(View view) {
+        Toast.makeText(MainActivity.this, "Loading...", Toast.LENGTH_SHORT).show();
+        removePolylines();
+        removeLocationUpdatesWithCallback();
+        NetworkRequestManager.getDrivingRoutePlanningResult(new LatLng(lat,lon), huawei,
+                new NetworkRequestManager.OnNetworkListener() {
+                    @Override
+                    public void requestSuccess(String result) {
+                        generateRoute(result);
+                    }
+
+                    @Override
+                    public void requestFail(String errorMsg) {
+                        Message msg = Message.obtain();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("errorMsg", errorMsg);
+                        msg.what = 1;
+                        msg.setData(bundle);
+                        mHandler.sendMessage(msg);
+                    }
+                });
+        requestLocationUpdatesWithCallback();
+    }
+
+    private void generateRoute(String json) {
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray routes = jsonObject.optJSONArray("routes");
+            if (null == routes || routes.length() == 0) {
+                return;
+            }
+            JSONObject route = routes.getJSONObject(0);
+
+            JSONObject bounds = route.optJSONObject("bounds");
+            if (null != bounds && bounds.has("southwest") && bounds.has("northeast")) {
+                JSONObject southwest = bounds.optJSONObject("southwest");
+                JSONObject northeast = bounds.optJSONObject("northeast");
+                LatLng sw = new LatLng(southwest.optDouble("lat"), southwest.optDouble("lng"));
+                LatLng ne = new LatLng(northeast.optDouble("lat"), northeast.optDouble("lng"));
+                mLatLngBounds = new LatLngBounds(sw, ne);
+            }
+
+            // get paths
+            JSONArray paths = route.optJSONArray("paths");
+            for (int i = 0; i < paths.length(); i++) {
+                JSONObject path = paths.optJSONObject(i);
+                List<LatLng> mPath = new ArrayList<>();
+
+                JSONArray steps = path.optJSONArray("steps");
+                for (int j = 0; j < steps.length(); j++) {
+                    JSONObject step = steps.optJSONObject(j);
+
+                    JSONArray polyline = step.optJSONArray("polyline");
+                    for (int k = 0; k < polyline.length(); k++) {
+                        if (j > 0 && k == 0) {
+                            continue;
+                        }
+                        JSONObject line = polyline.getJSONObject(k);
+                        double lat = line.optDouble("lat");
+                        double lng = line.optDouble("lng");
+                        LatLng latLng = new LatLng(lat, lng);
+                        mPath.add(latLng);
+                    }
+                }
+                mPaths.add(i, mPath);
+            }
+            mHandler.sendEmptyMessage(0);
+
+        } catch (JSONException e) {
+
+        }
+    }
+
+    private void renderRoute(List<List<LatLng>> paths, LatLngBounds latLngBounds) {
+        if (null == paths || paths.size() <= 0 || paths.get(0).size() <= 0) {
+            return;
+        }
+
+        for (int i = 0; i < paths.size(); i++) {
+            List<LatLng> path = paths.get(i);
+            PolylineOptions options = new PolylineOptions().color(Color.BLUE).width(5);
+            for (LatLng latLng : path) {
+                options.add(latLng);
+            }
+
+            Polyline polyline = hMap.addPolyline(options);
+            mPolylines.add(i, polyline);
+        }
+
+        addOriginMarker(paths.get(0).get(0));
+        addDestinationMarker(paths.get(0).get(paths.get(0).size() - 1));
+
+        if (null != latLngBounds) {
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(latLngBounds, 5);
+            hMap.moveCamera(cameraUpdate);
+        } else {
+            hMap.moveCamera(CameraUpdateFactory.newLatLngZoom(paths.get(0).get(0), 15));
+        }
+
+    }
+
+    private void addOriginMarker(LatLng latLng) {
+        if (null != mMarkerOrigin) {
+            mMarkerOrigin.remove();
+        }
+        mMarkerOrigin = hMap.addMarker(new MarkerOptions().position(latLng).title("Starting Point"));
+    }
+
+    private void addDestinationMarker(LatLng latLng) {
+        if (null != mMarkerDestination) {
+            mMarkerDestination.remove();
+        }
+        mMarkerDestination = hMap.addMarker(new MarkerOptions().position(latLng).title("Huawei Turkey Office"));
+    }
+
+    private void removePolylines() {
+        for (Polyline polyline : mPolylines) {
+            polyline.remove();
+        }
+
+        mPolylines.clear();
+        mPaths.clear();
+        mLatLngBounds = null;
+    }
+
+    public void CheckPermissions(){
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                String[] strings =
+                        {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+                ActivityCompat.requestPermissions(this, strings, 1);
+            }
+        } else {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this,
+                    "android.permission.ACCESS_BACKGROUND_LOCATION") != PackageManager.PERMISSION_GRANTED) {
+                String[] strings = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        "android.permission.ACCESS_BACKGROUND_LOCATION"};
+                ActivityCompat.requestPermissions(this, strings, 2);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions, @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                requestLocationUpdatesWithCallback();
+            }
+        }
+
+        if (requestCode == 2) {
+            if (grantResults.length > 2 && grantResults[2] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                requestLocationUpdatesWithCallback();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        removeLocationUpdatesWithCallback();
+        super.onDestroy();
+    }
+
+    private void removeLocationUpdatesWithCallback() {
+        try {
+            Task<Void> voidTask = mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
+            voidTask.addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Log.i(TAG, "removeLocationUpdatesWithCallback onSuccess");
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            Log.e(TAG, "removeLocationUpdatesWithCallback onFailure:" + e.getMessage());
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e(TAG, "removeLocationUpdatesWithCallback exception:" + e.getMessage());
+        }
+    }
+}
